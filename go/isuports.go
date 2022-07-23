2c02134b7e931364ebe5c6b8814e1d08c84bb463
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-23 15:23:57 +0900

    uuid v4

--- go/isuports.go
+++ go/isuports.go
@@ -107,7 +107,7 @@ func createTenantDB(id int64) error {
 
 // システム全体で一意なIDを生成する
 func dispenseID(ctx context.Context) (string, error) {
-	id, err := uuid.NewUUID()
+	id, err := uuid.NewRandom()
 	if err != nil {
 		return "", err
 	}
