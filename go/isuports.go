39f42f8b687e92a5d519904b8f839c13eaef1df2
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-23 14:56:35 +0900

    bulk insert

--- go/isuports.go
+++ go/isuports.go
@@ -1156,20 +1156,39 @@ func competitionScoreHandler(c echo.Context) error {
 	//); err != nil {
 	//	return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	//}
+	chunkSize := 1000
+	originalSql := "INSERT INTO player_score(id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES"
+	currentSql := originalSql
+	args := make([]interface{}, 0, len(playerScoreRows)*8)
 	for _, ps := range playerScoreRows {
-		if _, err := tenantDB.NamedExecContext(
-			ctx,
-			"INSERT INTO player_score(id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) "+
-				"VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at) "+
-				"ON CONFLICT(player_id, competition_id, tenant_id) DO UPDATE SET score=EXCLUDED.score, row_num=EXCLUDED.row_num, updated_at=EXCLUDED.updated_at",
-			ps,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
-				ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
-			)
+		currentSql += "(?, ?, ?, ?, ?, ?, ?, ?),"
+		args = append(args, ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt)
 
+		if len(args) >= chunkSize*8 {
+			currentSql = strings.TrimSuffix(currentSql, ",")
+			currentSql += " ON CONFLICT(player_id, competition_id, tenant_id) DO UPDATE SET score=EXCLUDED.score, row_num=EXCLUDED.row_num, updated_at=EXCLUDED.updated_at"
+			go func(currentSql string, ps []interface{}) {
+				_, err := tenantDB.Exec(currentSql, args...)
+				if err != nil {
+					log.Errorf("Async insert error: %w\n", err)
+				}
+			}(currentSql, args)
+			currentSql = originalSql
+			args = make([]interface{}, 0, chunkSize*8)
 		}
+		//if _, err := tenantDB.ExecContext(
+		//	ctx,
+		//	"INSERT INTO player_score(id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) "+
+		//		"VALUES (?, ?, ?, ?, ?, ?, ?, ?) "+
+		//		"ON CONFLICT(player_id, competition_id, tenant_id) DO UPDATE SET score=EXCLUDED.score, row_num=EXCLUDED.row_num, updated_at=EXCLUDED.updated_at",
+		//	ps,
+		//); err != nil {
+		//	return fmt.Errorf(
+		//		"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
+		//		ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
+		//	)
+		//
+		//}
 	}
 
 	return c.JSON(http.StatusOK, SuccessResult{
