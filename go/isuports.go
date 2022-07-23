ead042c8e53243130af3619d8ceebabf0bc29870
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-23 14:11:48 +0900

    wip

--- go/isuports.go
+++ go/isuports.go
@@ -1160,7 +1160,7 @@ func competitionScoreHandler(c echo.Context) error {
 		if _, err := tenantDB.NamedExecContext(
 			ctx,
 			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)"+
-				" ON DUPLICATE KEY UPDATE SET score=VALUES(score), row_num=VALUES(row_num), updated_at=VALUES(updated_at)",
+				" ON DUPLICATE KEY UPDATE score=VALUES(score), row_num=VALUES(row_num), updated_at=VALUES(updated_at)",
 			ps,
 		); err != nil {
 			return fmt.Errorf(
