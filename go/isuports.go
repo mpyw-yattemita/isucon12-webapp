233bcd874c272293ef408909c0952409665a2ac5
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-23 13:51:13 +0900

    replace into

--- go/isuports.go
+++ go/isuports.go
@@ -1148,18 +1148,18 @@ func competitionScoreHandler(c echo.Context) error {
 		})
 	}
 
-	if _, err := tenantDB.ExecContext(
-		ctx,
-		"DELETE FROM player_score WHERE tenant_id = ? AND competition_id = ?",
-		v.tenantID,
-		competitionID,
-	); err != nil {
-		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
-	}
+	//if _, err := tenantDB.ExecContext(
+	//	ctx,
+	//	"DELETE FROM player_score WHERE tenant_id = ? AND competition_id = ?",
+	//	v.tenantID,
+	//	competitionID,
+	//); err != nil {
+	//	return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
+	//}
 	for _, ps := range playerScoreRows {
 		if _, err := tenantDB.NamedExecContext(
 			ctx,
-			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
+			"REPLACE INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
 			ps,
 		); err != nil {
 			return fmt.Errorf(
