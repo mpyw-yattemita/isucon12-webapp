d4744c2cda3ec7167558cad0bce5f8cd672d7b8d
mpyw <ryosuke_i_628@yahoo.co.jp>
2022-07-23 14:37:07 +0900

    wip

--- go/isuports.go
+++ go/isuports.go
@@ -1157,12 +1157,12 @@ func competitionScoreHandler(c echo.Context) error {
 	//	return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	//}
 	for _, ps := range playerScoreRows {
-		if _, err := tenantDB.NamedExecContext(
+		if _, err := tenantDB.ExecContext(
 			ctx,
 			"INSERT INTO player_score(id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) "+
-				"VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at) "+
+				"VALUES (?, ?, ?, ?, ?, ?, ?, ?) "+
 				"ON DUPLICATE KEY UPDATE score=VALUES(score), row_num=VALUES(row_num), updated_at=VALUES(updated_at)",
-			ps,
+			ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt,
 		); err != nil {
 			return fmt.Errorf(
 				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
